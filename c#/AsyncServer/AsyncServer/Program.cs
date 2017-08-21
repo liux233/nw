f3844e4e89cece64be14494f820560d865fa1761
liux233 <m18064097027@163.com>
2017-08-21 18:17:38 +0800

    add AsyncServer

---
+++ c#/AsyncServer/AsyncServer/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace AsyncServer
+{
+    class Program
+    {
+        //Thread single
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        public static void StartListening()
+        {
+            byte[] bytes = new byte[1024];
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint point = new IPEndPoint(ip, 6688);
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(point);
+                listener.Listen(100);
+                while (true)
+                {
+                    allDone.Reset();
+                    Console.WriteLine("waiting for a connection...");
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+                    //wait until a connection is made before continuing
+                    allDone.WaitOne();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("\nPress Enter to continue...");
+            Console.Read();
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            allDone.Set();
+            Socket listener = (Socket)ar.AsyncState;
+
+            Socket handler = listener.EndAccept(ar);
+
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            string content = String.Empty;
+
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            int byteRead = handler.EndReceive(ar);
+            if (byteRead > 0)
+            {
+                state.sb.Append(Encoding.UTF8.GetString(state.buffer, 0, byteRead));
+
+                content = state.sb.ToString();
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    Console.WriteLine("Read {0} bytes from socket.\n Data: {1}", content.Length, content);
+                    Send(handler, content);
+                }
+                else
+                {
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            byte[] byteData = Encoding.UTF8.GetBytes(data);
+            handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket handler = (Socket)ar.AsyncState;
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0}  bytes to client", bytesSent);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StartListening();
+
+        }
+    }
+}
